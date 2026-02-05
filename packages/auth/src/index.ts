aa843db97b954c43269f9d8e0788ce10bccd9716
Michael Henke <henke.mike@gmail.com>
2026-02-05 09:01:16 -0600

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { db } from "@start-spec-driving/db";
+import * as schema from "@start-spec-driving/db/schema/auth";
+import { env } from "@start-spec-driving/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [tanstackStartCookies()],
+});
